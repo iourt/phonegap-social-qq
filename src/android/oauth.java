e8624245f5e311648384b64b8ba2808ff76d3b9b
Luncher <huasuolinchen@163.com>
2015-04-06 18:37:27 +0800

    fix get user info

--- src/android/oauth.java
+++ src/android/oauth.java
@@ -17,6 +17,9 @@ import android.widget.Toast;
 import com.tencent.tauth.IUiListener;
 import com.tencent.tauth.UiError;
 import com.tencent.tauth.Tencent;
+import com.tencent.tauth.Tencent;
+import com.tencent.connect.UserInfo;
+import com.tencent.connect.common.Constants;
 
 public class oauth extends CordovaPlugin {
 	private static final String ACTION_SAY_HELLO = "say-hello";	
@@ -24,7 +27,8 @@ public class oauth extends CordovaPlugin {
 	private static final String ACTION_QQ_SHARED = "qq-shared";	
 
 	public  static final String APPID = "101195786";
-	private Tencent m_tHandle = null; 
+	private Tencent  m_tHandle = null; 
+	private UserInfo m_userInfo= null; 
 	private CallbackContext m_cbCtx = null;
 
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -41,6 +45,9 @@ public class oauth extends CordovaPlugin {
 		else if(action.equals("qq-shared")) {
 			this.qqShared();
 		}
+		else if(action.equals("get-userinfo")) {
+			//this.getUserInfo(args);
+		}
 		return true;
 	}
 	
@@ -49,7 +56,29 @@ public class oauth extends CordovaPlugin {
 	}
 
 	private void qqLogout() {
-		//TODO
+		Context context = this.cordova.getActivity().getApplicationContext();
+		m_tHandle.logout(context);
+		m_cbCtx.success();
+	}
+	
+	final private void getUserInfo() {
+		final Activity activity = this.cordova.getActivity();
+		//Context context = this.cordova.getActivity().getApplicationContext();
+		//m_tHandle = Tencent.createInstance(APPID, context);	
+		//JSONObject jsonObj = args.getJSONObject(0);
+		//String uid = jsonObj.getString("uid"); 
+		//String token = jsonObj.getString("token"); 
+		//m_tHandle.setOpenId(uid);
+		//m_tHandle.setAccessToken(token);
+
+		final IUiListener listener2 = new BaseUiListener() {
+			protected void doComplete(JSONObject values) {
+				m_cbCtx.success(values);
+				//m_cbCtx.success("hello owrld!");
+			}
+		};
+		m_userInfo = new UserInfo(activity, m_tHandle.getQQToken());
+		m_userInfo.getUserInfo(listener2);
 	}
 
 	private void qqLogin() {
@@ -59,6 +88,18 @@ public class oauth extends CordovaPlugin {
 
 		final IUiListener listener = new BaseUiListener() {
 			protected void doComplete(JSONObject values) {
+				String uid=m_tHandle.getOpenId();
+				String token=m_tHandle.getAccessToken();
+				
+				//m_cbCtx.success("hello");
+				getUserInfo();				
+			//	new Thread() {
+			//		public void run() {
+			//			JSONObject res = new JSONObject();
+			//			res = m_tHandle.request(Constants.GRAPH_SIMPLE_USER_INFO, null, Constants.HTTP_GET);
+			//			m_cbCtx.success(res);
+			//		}
+			//	}.start();
 			}
 		};
 
@@ -71,24 +112,26 @@ public class oauth extends CordovaPlugin {
 
 	private class BaseUiListener implements IUiListener {
 		public void onComplete(Object response) {
-			String uid=m_tHandle.getOpenId();
-			String token=m_tHandle.getAccessToken();
+			doComplete((JSONObject)response);
+			//String uid=m_tHandle.getOpenId();
+			//String token=m_tHandle.getAccessToken();
 
-			JSONObject res = new JSONObject();
-			try {
-				res.put("uid", uid);
-				res.put("token", token);
-				m_cbCtx.success(res);
-			}
-			catch(JSONException e) {
-				m_cbCtx.error("error");	
-			}
+			//JSONObject res = new JSONObject();
+			//try {
+			//	res.put("uid", uid);
+			//	res.put("token", token);
+			//	m_cbCtx.success(res);
+			//}
+			//catch(JSONException e) {
+			//	m_cbCtx.error("error");	
+			//}
 		}
 
 		protected void doComplete(JSONObject values) {
 		}
 
 		public void onError(UiError e) {
+			m_cbCtx.error("message:"+ e.errorMessage + "code:" + e.errorCode + "detail:" + e.errorDetail);
 		}
 
 		public void onCancel() {
--- www/oauth.js
+++ www/oauth.js
@@ -12,6 +12,10 @@ exports.qqLogout = function(success, error) {
 	exec(success, error, "oauth", "qq-logout", []);
 };
 
+exports.getUserInfo = function(options, success, error) {
+	exec(success, error, "oauth", "get-userinfo", [options]);
+};
+
 exports.qqShared = function(success, error) {
 	exec(success, error, "oauth", "qq-shared", []);
 };
